8fdf83f3329e9410530b80de5eaf09ca2376686e
kmd-htsh <job@kmd-htsh.com>
2020-10-27 08:41:07 +0900

    ビルドエラー解消。

--- app/src/main/java/com/kmdhtsh/qiita0lgtmviewer/Extensions.kt
+++ app/src/main/java/com/kmdhtsh/qiita0lgtmviewer/Extensions.kt
@@ -4,7 +4,7 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
-@BindingAdapter("bind:imageUrl")
+@BindingAdapter("imageUrl")
 fun ImageView.loadImage(url: String) {
     Glide.with(context).load(url).into(this)
 }
